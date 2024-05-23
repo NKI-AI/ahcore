655cd03cb082d6c50e749f7ad50e5ea7eb5043e1
Ajey Pai K <ajeypie@gmail.com>
2024-05-23 20:49:12 +0200

    Refactoring

--- ahcore/models/jit_model.py
+++ ahcore/models/jit_model.py
@@ -1,55 +1,102 @@
 from pathlib import Path
-from typing import Any
+from typing import Any, Callable
 
 import torch
 from torch.jit import ScriptModule, load
 from torch.nn import Module
 
-from ahcore.utils.types import FMEmbedType, JitOutputMode
+from ahcore.utils.types import AllModelTypes, AllOutputModes, FMEmbedType, FMOutputMode, JitModelType, JitOutputMode
 
 
 class AhcoreJitModel(ScriptModule):
-    def __init__(self, model: ScriptModule, output_mode: str) -> None:
+    def __init__(self, model: ScriptModule, model_type: AllModelTypes, output_mode: AllOutputModes) -> None:
         super().__init__()  # type: ignore
         self._model = model
-        if output_mode == JitOutputMode.EMBED_CLS_ONLY:
-            output_function = self.cls_only
-        elif output_mode == JitOutputMode.EMBED_PATCH_ONLY:
-            output_function = self.patch_only
-        elif output_mode == JitOutputMode.EMBED_CONCAT:
-            output_function = self.concat_embedding
-        elif output_mode == JitOutputMode.SEGMENTATION:
-            output_function = self.segmentation_map
-        else:
-            raise ValueError(f"Unsupported output mode: {self.output_mode}")
-
-        self._forward_function = output_function
+        self._model_type = model_type
+        self._output_mode = output_mode
+        self._forward_function: Callable[[torch.Tensor], torch.Tensor]
 
     @classmethod
-    def from_jit_path(cls, jit_path: Path, output_mode: str) -> Any:
+    def from_jit_path(cls, jit_path: Path, model_type: AllModelTypes, output_mode: AllOutputModes) -> Any:
         model = load(jit_path)  # type: ignore
-        return cls(model, output_mode)
-
-    def segmentation_map(self, x: torch.Tensor) -> torch.Tensor:
-        return self._model(x)
-
-    def cls_only(self, x: torch.Tensor) -> torch.Tensor:
-        return self._model(x)[FMEmbedType.CLS_TOKEN]
-
-    def patch_only(self, x: torch.Tensor) -> torch.Tensor:
-        return self._model(x)[FMEmbedType.PATCH_TOKEN]
-
-    def concat_embedding(self, x: torch.Tensor) -> torch.Tensor:
-        output = self._model(x)
-        cls_token = output[FMEmbedType.CLS_TOKEN].unsqueeze(1)
-        mean_patch_token = output[FMEmbedType.PATCH_TOKEN].mean(dim=1).unsqueeze(1)
-        tokens = torch.cat([cls_token, mean_patch_token], dim=1)
-        output = tokens.mean(dim=1)
-        return output
+        return cls(model, model_type, output_mode)
 
     def extend_model(self, modules: dict[str, Module]) -> None:
         for key, value in modules.items():
             self._model.add_module(name=key, module=value)
+
+    def forward(self, x: torch.Tensor) -> Any:
+        return self._forward_function(x)
+
+
+class FoundationalModel(AhcoreJitModel):
+    """
+    This class is a wrapper for the foundation models in Ahcore.
+    It provides a general interface for the jit compiled foundation models that can be used in different contexts.
+    """
+
+    def __init__(self, model: ScriptModule, model_type: AllModelTypes, output_mode: AllOutputModes) -> None:
+        super().__init__(model=model, model_type=JitModelType.FoundationModel, output_mode=output_mode)
+        self._fm_type = model_type
+        self._fm_embed_type = FMEmbedType.for_type(self._fm_type)
+        if self._output_mode == FMOutputMode.EMBED_CLS_ONLY:
+            self._forward_function = self._cls_only
+        elif self._output_mode == FMOutputMode.EMBED_PATCH_ONLY:
+            self._forward_function = self._patch_only
+        elif self._output_mode == FMOutputMode.EMBED_CONCAT:
+            self._forward_function = self._concat_embedding
+        elif self._output_mode == FMOutputMode.GENERAL_EMBEDDING:
+            self._forward_function = self._general_embedding
+        else:
+            raise NotImplementedError(f"Output mode {self._output_mode} is not supported for {self.__class__.__name__}")
+
+    @classmethod
+    def from_jit_path(cls, jit_path: Path, model_type: AllModelTypes, output_mode: AllOutputModes) -> Any:
+        model = load(jit_path)  # type: ignore
+        return cls(model, model_type, output_mode)
+
+    def _cls_only(self, x: torch.Tensor) -> Any:
+        return self._model(x)[FMEmbedType.CLS_TOKEN.value]
+
+    def _patch_only(self, x: torch.Tensor) -> Any:
+        return self._model(x)[FMEmbedType.PATCH_TOKEN.value]
+
+    def _concat_embedding(self, x: torch.Tensor) -> Any:
+        output = self._model(x)
+        cls_token = output[FMEmbedType.CLS_TOKEN.value].unsqueeze(1)
+        mean_patch_token = output[FMEmbedType.PATCH_TOKEN.value].mean(dim=1).unsqueeze(1)
+        tokens = torch.cat([cls_token, mean_patch_token], dim=1)
+        output = tokens.mean(dim=1)
+        return output
+
+    def _general_embedding(self, x: torch.Tensor) -> Any:
+        # This function is for the foundation models that do not have specific embedding outputs.
+        return self._model(x)
+
+    def forward(self, x: torch.Tensor) -> Any:
+        return self._forward_function(x)
+
+
+class SegmentationModel(AhcoreJitModel):
+    """
+    This class is a wrapper for the segmentation models in Ahcore.
+    It provides a general interface for the jit compiled segmentation models that can be used in different contexts.
+    """
+
+    def __init__(self, model: ScriptModule, model_type: AllModelTypes, output_mode: AllOutputModes) -> None:
+        super().__init__(model=model, model_type=JitModelType.SegmentationModel, output_mode=output_mode)
+        if self._output_mode == JitOutputMode.SEGMENTATION:
+            self._forward_function = self._segmentation
+        else:
+            raise NotImplementedError(f"Output mode {self._output_mode} is not supported for {self.__class__.__name__}")
+
+    @classmethod
+    def from_jit_path(cls, jit_path: Path, model_type: AllModelTypes, output_mode: AllOutputModes) -> Any:
+        model = load(jit_path)  # type: ignore
+        return cls(model, model_type, output_mode)
+
+    def _segmentation(self, x: torch.Tensor) -> Any:
+        return self._model(x)
 
     def forward(self, x: torch.Tensor) -> Any:
         return self._forward_function(x)
