25d36a022dec24bb42df00f2d2b07a23b408b6dc
Ajey Pai K <ajeypie@gmail.com>
2024-05-28 15:59:16 +0200

    Refactoring

--- ahcore/models/feature_extractors.py
+++ ahcore/models/feature_extractors.py
@@ -13,7 +13,7 @@ class DinoV2JitModel(AhcoreJitModel):
     This class is a wrapper for the DinoV2 foundation model in Ahcore.
     """
 
-    def __init__(self, model: ScriptModule, embed_mode: OutputModeBase) -> None:
+    def __init__(self, model: ScriptModule, output_mode: OutputModeBase) -> None:
         """
         Constructor for the DinoV2 class.
         This can be a feature extractor based on very large pre-trained model.
@@ -23,19 +23,18 @@ class DinoV2JitModel(AhcoreJitModel):
         model : ScriptModule
             The jit compiled model.
 
-        embed_mode : ViTEmbedMode
+        output_mode : OutputModeBase
             The output mode of the model. This is used to determine the forward function of the model.
 
         Returns
         -------
         None
         """
-        super().__init__(model=model, output_mode=embed_mode)
-        self._embed_mode = embed_mode
+        super().__init__(model=model, output_mode=output_mode)
         self._set_forward_function()
 
     def _set_forward_function(self) -> None:
-        self._forward_function = ViTEmbed(self._embed_mode).embed_fn
+        self._forward_function = ViTEmbed(self._output_mode).embed_fn
 
     def forward(self, x: torch.Tensor) -> Any:
         output = self._model(x)
