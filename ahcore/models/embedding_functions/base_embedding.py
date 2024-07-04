f5af5c4558ae2ab36588ff2d25e58f60219ee73f
Ajey Pai K <ajeypie@gmail.com>
2024-07-04 16:11:06 +0200

    Typing and model config

--- ahcore/models/embedding_functions/base_embedding.py
+++ ahcore/models/embedding_functions/base_embedding.py
@@ -6,7 +6,7 @@ import torch
 
 class AhCoreFeatureEmbedding(ABC):
     def __init__(self) -> None:
-        self.embed_fn: Callable[[torch.Tensor, Optional[torch.Tensor]], torch.Tensor]
+        self.embed_fn: Callable[[torch.Tensor, torch.Tensor | None], torch.Tensor]
 
     @abstractmethod
     def _set_embed_function(self) -> None:
