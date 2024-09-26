64269354c6a6c909732d3a1561736b939eaf6824
Ajey Pai K <ajeypie@gmail.com>
2024-09-26 23:38:04 +0200

    some cleaning

--- ahcore/callbacks/log_images_callback.py
+++ ahcore/callbacks/log_images_callback.py
@@ -1,21 +1,21 @@
-import pytorch_lightning as pl
+from typing import Any, List, Tuple
+
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.colors import to_rgb
-
-from typing import List, Tuple, Any
+import pytorch_lightning as pl
 import torch
 import torch.nn.functional as F
-from ahcore.metrics.metrics import _compute_dice
+from matplotlib.colors import to_rgb
 
+from ahcore.metrics.metrics import _compute_dice
 from ahcore.utils.types import ScannerEnum
 
 
 def get_sample_wise_dice_components(
-        predictions: torch.Tensor,
-        target: torch.Tensor,
-        roi: torch.Tensor | None,
-        num_classes: int,
+    predictions: torch.Tensor,
+    target: torch.Tensor,
+    roi: torch.Tensor | None,
+    num_classes: int,
 ) -> List[List[Tuple[torch.Tensor, torch.Tensor]]]:
     # Apply softmax along the class dimension
     soft_predictions = F.softmax(predictions, dim=1)
@@ -42,17 +42,13 @@ def get_sample_wise_dice_components(
                 # Apply ROI if it's provided
                 curr_roi = roi[batch_idx].squeeze(0)  # Adjust ROI for current sample
                 intersection = torch.sum((curr_predictions * curr_target) * curr_roi, dim=(0, 1))
-                cardinality = (
-                        torch.sum(curr_predictions * curr_roi, dim=(0, 1)) +
-                        torch.sum(curr_target * curr_roi, dim=(0, 1))
+                cardinality = torch.sum(curr_predictions * curr_roi, dim=(0, 1)) + torch.sum(
+                    curr_target * curr_roi, dim=(0, 1)
                 )
             else:
                 # No ROI, just compute intersection and cardinality
                 intersection = torch.sum(curr_predictions * curr_target, dim=(0, 1))
-                cardinality = (
-                        torch.sum(curr_predictions, dim=(0, 1)) +
-                        torch.sum(curr_target, dim=(0, 1))
-                )
+                cardinality = torch.sum(curr_predictions, dim=(0, 1)) + torch.sum(curr_target, dim=(0, 1))
 
             batch_dice_components.append((intersection, cardinality))
 
@@ -61,9 +57,11 @@ def get_sample_wise_dice_components(
     return dice_components
 
 
-def get_sample_wise_dice(outputs: dict[str, Any], batch: dict[str, Any], roi: torch.Tensor, num_classes: int) -> List[dict[int, float]]:
+def get_sample_wise_dice(
+    outputs: dict[str, Any], batch: dict[str, Any], roi: torch.Tensor, num_classes: int
+) -> List[dict[int, float]]:
     dices = []
-    dice_components = get_sample_wise_dice_components(outputs['prediction'], batch['target'], roi, num_classes)
+    dice_components = get_sample_wise_dice_components(outputs["prediction"], batch["target"], roi, num_classes)
     for samplewise_dice_components in dice_components:
         classwise_dices_for_a_sample = {1: 0, 2: 0, 3: 0}
         for class_idx in range(num_classes):
@@ -78,16 +76,20 @@ def get_sample_wise_dice(outputs: dict[str, Any], batch: dict[str, Any], roi: to
 
 def _extract_scanner_name(path) -> str:
     # Extract file extension
-    extension = path.split('.')[-1]
+    extension = path.split(".")[-1]
     # Use the ScannerEnum to get the scanner name
     return ScannerEnum.get_scanner_name(extension)
 
 
 class LogImagesCallback(pl.Callback):
-    def __init__(self, color_map: dict[int, str], num_classes: int,
-                 plot_dice: bool = True,
-                 plot_scanner_wise: bool = False,
-                 plot_every_n_epochs: int = 10):
+    def __init__(
+        self,
+        color_map: dict[int, str],
+        num_classes: int,
+        plot_dice: bool = True,
+        plot_scanner_wise: bool = False,
+        plot_every_n_epochs: int = 10,
+    ):
         super().__init__()
         self.color_map = {k: np.array(to_rgb(v)) * 255 for k, v in color_map.items()}
         self._num_classes = num_classes
@@ -97,11 +99,10 @@ class LogImagesCallback(pl.Callback):
         self._plot_dice = plot_dice
 
     def on_validation_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx=0) -> None:
-
         if trainer.current_epoch % self._plot_every_n_epochs == 0:
-            val_images = batch['image']
-            roi = batch['roi']
-            path = batch['path'][0]
+            val_images = batch["image"]
+            roi = batch["roi"]
+            path = batch["path"][0]
 
             if self._plot_scanner_wise:
                 scanner_name = _extract_scanner_name(path)
@@ -115,17 +116,26 @@ class LogImagesCallback(pl.Callback):
 
             val_images_numpy = val_images.permute(0, 2, 3, 1).detach().cpu().numpy()
             val_images_numpy = (val_images_numpy - val_images_numpy.min()) / (
-                        val_images_numpy.max() - val_images_numpy.min())
+                val_images_numpy.max() - val_images_numpy.min()
+            )
 
-            val_predictions = outputs['prediction']
+            val_predictions = outputs["prediction"]
             val_predictions_numpy = val_predictions.permute(0, 2, 3, 1).detach().cpu().numpy()
 
-            val_targets = batch['target']
+            val_targets = batch["target"]
             val_targets_numpy = val_targets.permute(0, 2, 3, 1).detach().cpu().numpy()
 
             if scanner_name not in self._already_seen_scanner:
-                self._plot_and_log(val_images_numpy, val_predictions_numpy, val_targets_numpy, pl_module,
-                                trainer.global_step, batch_idx, scanner_name, dices)
+                self._plot_and_log(
+                    val_images_numpy,
+                    val_predictions_numpy,
+                    val_targets_numpy,
+                    pl_module,
+                    trainer.global_step,
+                    batch_idx,
+                    scanner_name,
+                    dices,
+                )
 
             if self._plot_scanner_wise and scanner_name is not None:
                 self._already_seen_scanner.append(scanner_name)
@@ -133,8 +143,17 @@ class LogImagesCallback(pl.Callback):
     def on_validation_epoch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule") -> None:
         self._already_seen_scanner = []
 
-    def _plot_and_log(self, images_numpy, predictions_numpy, targets_numpy, pl_module, step, batch_idx, scanner_name,
-                      dices) -> None:
+    def _plot_and_log(
+        self,
+        images_numpy: np.ndarray,
+        predictions_numpy: np.ndarray,
+        targets_numpy: np.ndarray,
+        pl_module: "pl.LightningModule",
+        step: int,
+        batch_idx: int,
+        scanner_name: str,
+        dices: List[dict[int, float]],
+    ) -> None:
         batch_size = images_numpy.shape[0]
         figure = plt.figure(figsize=(15, batch_size * 5))
         for i in range(batch_size):
@@ -162,16 +181,12 @@ class LogImagesCallback(pl.Callback):
             plt.tight_layout()
 
         artifact_file_name = f"validation_global_step{step:03d}_batch{batch_idx:03d}.png"
-        pl_module.logger.experiment.log_figure(
-            pl_module.logger.run_id,
-            figure,
-            artifact_file=artifact_file_name
-        )
+        pl_module.logger.experiment.log_figure(pl_module.logger.run_id, figure, artifact_file=artifact_file_name)
         plt.close()
 
 
-def apply_color_map(image, color_map, num_classes):
+def apply_color_map(image, color_map, num_classes) -> np.ndarray:
     colored_image = np.zeros((*image.shape, 3), dtype=np.uint8)
-    for i in range(1, num_classes-1):
+    for i in range(1, num_classes - 1):
         colored_image[image == i] = color_map[i]
     return colored_image
